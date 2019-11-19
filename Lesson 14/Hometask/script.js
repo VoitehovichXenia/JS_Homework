4f38b33f44d8736acc91b1bf19742ad8328df2aa
VoitehovichXenia <voitehovichkg@gmail.com>
2019-11-20 01:20:18 +0300

    first commit

--- Lesson 14/Hometask/script.js
+++ Lesson 14/Hometask/script.js
@@ -1,20 +1,19 @@
-// Добавить на страницу два поля для ввода - X и Y и кнопку "Create".
-//     Если хотя бы одно из полей пустое - кнопка дизэйблится (делать по keyup).
-// Поля должны принимать только числа от 1 до 10, в противном случае должно выводиться сообщение об ошибке (делать по click).
-// По клику на кнопку должна отрисоваться шахматная доска с размерами X по горизонтали и Y по вертикали.
-//     При введении значений X и Y заново таблица должна корректно перерисоваться.
-//     По клику на любое поле доски - цвета всех полей должны изменяться на противоположные.
-
-var inputElements = document.getElementsByClassName('input'),
-    content = document.getElementsByClassName('content'),
+var inputElements = document.getElementsByTagName('input'),
+    body = document.getElementsByTagName('body'),
     button = document.getElementsByTagName('button');
 
 button[0].setAttribute('disabled', 'true');
 button[0].addEventListener('click', chekInputValue, false);
 button[0].addEventListener('click', drawChessPad,false);
 
+function clearInputs() {
+    for (var m = 0; m < inputElements.length;m++){
+        inputElements[m].value = null;
+    }
+}
+
 for (var i = 0; i < inputElements.length; i++){
-    inputElements[i] = null;
+    clearInputs();
     inputElements[i].addEventListener('keyup',buttonDisableValue,false);
 }
 
@@ -24,11 +23,11 @@ function buttonDisableValue(){
 
 function chekInputValue() {
     for (var j = 0; j < inputElements.length; j++){
-        if (!(typeof inputElements[j].value === 'number') || inputElements[j].value > 10 && inputElements[j].value < 1){
-            alert ('Please white in the forms below number from 1 to 10!');
-            button[0].disabled = true;
-            break;
-        }
+        if (inputElements[j].value <= 10 && inputElements[j].value >= 1) break;
+        button[0].disabled = true;
+        clearInputs();
+        alert ('Please white in the forms below number from 1 to 10!');
+        break;
     }
 }
 
@@ -62,11 +61,9 @@ function drawChessPad() {
 
     }
 
-    for (var l = 0; l < inputElements.length; l++){
-        inputElements[l] = null;
-    }
+    clearInputs();
 
-    content[0].appendChild(table);
+    body[0].appendChild(table);
 
     table.onclick = function (event) {
 
